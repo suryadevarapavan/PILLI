dcd4ebe9599dfa44cc5bce6b8e703928e94599f1
P1ttadu <suryadevarapavan770@gmail.com>
2025-01-21 15:33:08 +0530

    Update insta_1.ino
    Removed unwanted statements and add comments to specify their respective tasks. Added variable usage in source code

--- insta_1.ino
+++ insta_1.ino
@@ -1,29 +1,27 @@
 void blink1(){
   pinMode(12,OUTPUT);
   digitalWrite(12, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(12, LOW);
-  delay(500);
-  Serial.print("\nIt's Alive!!");
-  
+  delay(100);
+  //This function blinks second led which represents pilli is awake.
+
 }
 
 void blink(){
   pinMode(13,OUTPUT);
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(100);
   digitalWrite(13, LOW);
-  delay(500);
-  //Serial.print("\nIt's Alive!!");
+  delay(100);
+  //This function blinks second led which represents pilli is sleeping.
   
 }
-const int TRIG_PIN = 6; 
-const int ECHO_PIN = 7; 
-const int LED_PIN  = 13; 
-const int DISTANCE_THRESHOLD = 20; 
+const int TRIG_PIN = 6; //variable is used for specifying trigger pin of ultrasonic sensor to ardunio board.
+const int ECHO_PIN = 7; //variable is used for specifying echo pin of ultrasonic sensor to ardunio board.
+const int DISTANCE_THRESHOLD = 10; // variable is used for checking calculated distance. 
 
-
-float duration_us, distance_cm;
+float duration_us, distance_cm; //Variables are used to caluclate distance.
 
 void setup() {
   Serial.begin (9600);       
@@ -41,17 +39,10 @@ void loop() {
   
   duration_us = pulseIn(ECHO_PIN, HIGH);
   
-  distance_cm = 0.017 * duration_us;
+  distance_cm = 0.017 * duration_us; // Used to calculte distance.
 
   if(distance_cm < DISTANCE_THRESHOLD)
     blink();
   else
     blink1();  
-
- 
-  Serial.print("distance: ");
-  Serial.print(distance_cm);
-  Serial.println(" cm");
-
-  delay(500);
 }
