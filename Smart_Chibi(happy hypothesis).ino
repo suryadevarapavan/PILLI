606e2d54b95fb1c2b30da797f6ce0ba7408bc70d
Suryadevara Pavan <suryadevarapavan770@gmail.com>
2025-02-18 08:42:14 +0530

    Update Smart_Chibi(happy hypothesis).ino

--- Smart_Chibi(happy hypothesis).ino
+++ Smart_Chibi(happy hypothesis).ino
@@ -26,7 +26,7 @@ const char* trainingData[numExamples][numAttributes] = {
 };
 const bool isPositive[numExamples] = {true, true, false, true};
 char hypothesis[numAttributes][10];
-char inputHypothesis[numAttributes][10] = { "eat", "play", "fetch" }; // Example input hypothesis
+char inputHypothesis[numAttributes][10] = { "eat", "play", "fetch" }; // Happy hypothesis
 
 void initializeHypothesis() {
     for (int i = 0; i < numAttributes; i++) {
